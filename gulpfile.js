9382e27e3164e7e4aa911362e40e00bb6e654df3
Nick Stuart <nick@portlandwebworks.com>
2016-06-03 09:42:52 -0400

    Formatting plugin in gulp to complain more.

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var ngmin = require('gulp-ng-annotate');
 var minifyCss = require('gulp-clean-css');
 var sass = require('gulp-sass');
 var jshint = require('gulp-jshint');
+var lintspaces = require('gulp-lintspaces');
 var stylish = require('jshint-stylish');
 var karma = require('gulp-karma');
 var flatten = require('gulp-flatten');
@@ -17,56 +18,62 @@ var sourcemap = require('gulp-sourcemaps');
 
 gulp.task('convert-sass', function () {
   return gulp.src('src/main/webapp/app/sass/*.scss')
-    .pipe(sourcemap.init())
-    .pipe(sass({
-      outputStyle: 'compressed',
-      includePaths: [
-        'src/main/webapp/bower_components/bootstrap-sass-official/assets/stylesheets'
-      ],
-      sourcemaps: true
-    }))
-    .pipe(sourcemap.write('./'))
-    .pipe(gulp.dest('src/main/webapp/app/css/'));
+          .pipe(sourcemap.init())
+          .pipe(sass({
+            outputStyle: 'compressed',
+            includePaths: [
+              'src/main/webapp/bower_components/bootstrap-sass-official/assets/stylesheets'
+            ],
+            sourcemaps: true
+          }))
+          .pipe(sourcemap.write('./'))
+          .pipe(gulp.dest('src/main/webapp/app/css/'));
 });
 
 
 gulp.task('test', function () {
   return gulp.src('./blah')
-    .pipe(karma({configFile: 'karma.conf.js', action: 'run'}))
-    .on('error', function (err) {
-      throw err;
-    });
+          .pipe(karma({configFile: 'karma.conf.js', action: 'run'}))
+          .on('error', function (err) {
+            throw err;
+          });
 });
 
 gulp.task('test-watch', function () {
   return gulp.src('./blah')
-    .pipe(karma({
-      configFile: 'karma.conf.js',
-      action: 'watch'
-    }));
+          .pipe(karma({
+            configFile: 'karma.conf.js',
+            action: 'watch'
+          }));
 });
 
-gulp.task('usemin', ['scripts', 'convert-sass'], function () {
+gulp.task('usemin', ['scripts', 'formatting', 'convert-sass'], function () {
   return gulp.src('**/*.jsp', {cwd: 'src/main/webapp/'})
-    .pipe(usemin({
-      externalJs: [ngmin(), uglify()],
-      appJs: [ngmin(), uglify()]
-    }))
-    .pipe(gulp.dest('target/chhs-frontend/'));
+          .pipe(usemin({
+            externalJs: [ngmin(), uglify()],
+            appJs: [ngmin(), uglify()]
+          }))
+          .pipe(gulp.dest('target/chhs-frontend/'));
 });
 
 
 gulp.task('copy-fonts', [], function () {
   return gulp.src(['src/main/webapp/fonts/*.{ttf,woff,woff2,eot,svg}', 'src/main/webapp/bower_components/**/*.{ttf,woff,woff2,eot,svg}'])
-    .pipe(flatten())
-    .pipe(gulp.dest('target/frontend-app/fonts'));
+          .pipe(flatten())
+          .pipe(gulp.dest('target/frontend-app/fonts'));
 });
 
 gulp.task('scripts', function () {
   return gulp.src('app/**/*.js', {cwd: 'src/main/webapp/'})
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish));
+          .pipe(jshint())
+          .pipe(jshint.reporter(stylish))
 });
+
+gulp.task('formatting', function () {
+  return gulp.src(['app/**/*.js', 'app/**/*.scss', 'app/**/*.jsp'], {cwd: 'src/main/webapp/'})
+          .pipe(lintspaces({editorconfig: '.editorconfig'}))
+          .pipe(lintspaces.reporter(stylish));
+})
 
 gulp.task('watch', function () {
   gulp.watch('src/main/webapp/app/sass/**/*.scss', ['convert-sass']);
@@ -75,7 +82,7 @@ gulp.task('watch', function () {
 
 gulp.task('ci', ['default']);
 
-gulp.task('default', ['scripts', 'convert-sass']);
+gulp.task('default', ['scripts', 'formatting', 'convert-sass']);
 
 gulp.task('release', ['usemin', 'remove-bower'], function () {
   return cdned('');
@@ -90,21 +97,21 @@ gulp.task('remove-bower', function () {
 function cdned(cdn) {
   var cssFilter = filter('**/*.css', {restore: true});
   var jsFilter = filter('**/*.js', {restore: true});
-  return gulp.src(['**/*.js', '**/*.css', '**/index.jsp', '!bower_components',], {cwd: 'target/chhs-frontend/'})
-    .pipe(jsFilter)
-    .pipe(rev())
-    .pipe(jsFilter.restore)
-    .pipe(cssFilter)
-    .pipe(rev())
-    .pipe(cssFilter.restore)
-    .pipe(revReplace({replaceInExtensions: ['.html', '.jsp']}))
-    .pipe(cdnizer({
-      defaultCDNBase: cdn,
-      relativeRoot: '/',
-      files: [
-        '**/*.js',
-        '**/styles.css'
-      ]
-    }))
-    .pipe(gulp.dest('target/chhs-frontend/'));
+  return gulp.src(['**/*.js', '**/*.css', '**/index.jsp', '!bower_components', ], {cwd: 'target/chhs-frontend/'})
+          .pipe(jsFilter)
+          .pipe(rev())
+          .pipe(jsFilter.restore)
+          .pipe(cssFilter)
+          .pipe(rev())
+          .pipe(cssFilter.restore)
+          .pipe(revReplace({replaceInExtensions: ['.html', '.jsp']}))
+          .pipe(cdnizer({
+            defaultCDNBase: cdn,
+            relativeRoot: '/',
+            files: [
+              '**/*.js',
+              '**/styles.css'
+            ]
+          }))
+          .pipe(gulp.dest('target/chhs-frontend/'));
 }
--- src/main/webapp/app/application.js
+++ src/main/webapp/app/application.js
@@ -2,7 +2,6 @@
 
 angular.module('chhs', ['ngCookies', 'ngRoute', 'uiGmapgoogle-maps'])
   .run(function ($cookies, $http, $log) {
-
     if ($cookies.get('Token')) {
       $log.debug('Auth token found, adding to all requests.');
       $http.defaults.headers.common.Token = $cookies.get('Token');
