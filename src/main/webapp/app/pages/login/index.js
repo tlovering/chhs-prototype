294f5fcd848c0a80ae436e060c41900e2030fe24
rcharow <rachel.charow@portlandwebworks.com>
2016-06-04 09:18:53 -0400

    Add login functionality to login controller

--- src/main/webapp/app/pages/login/index.js
+++ src/main/webapp/app/pages/login/index.js
@@ -1,5 +1,18 @@
 'use strict';
 
-angular.module('chhs').controller('loginCtrl', function ($http, $log) {
+angular.module('chhs').controller('loginCtrl', function ($http, $log, $location, Auth) {
 
+  var login = this;
+
+
+  this.login = function () {
+    Auth.login(login.auth)
+      .then(function () {
+        $location.path('home');
+        login.error = false;
+      }, function () {
+        login.error = true;
+
+      });
+  };
 });
