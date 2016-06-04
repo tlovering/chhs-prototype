f7b8f225cace60aa77fc476bbca5ccbbb1d7492d
rcharow <rachel.charow@portlandwebworks.com>
2016-06-04 17:22:10 -0400

    Use Account factory instead of Auth for account info

--- src/main/webapp/app/pages/home/index.js
+++ src/main/webapp/app/pages/home/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
-angular.module('chhs').controller('homeCtrl', function ($log, Auth) {
+angular.module('chhs').controller('homeCtrl', function ($log, Account) {
 
   var home = this;
 
-  Auth.getUserAccount()
+  Account.getUserAccount()
     .then(function (data) {
       home.account = data;
     })
