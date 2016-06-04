1c6cda35a884203c10a947b1e0182f8d334d816c
rcharow <rachel.charow@portlandwebworks.com>
2016-06-04 09:17:39 -0400

    Remove login function from home controller

--- src/main/webapp/app/pages/home/index.js
+++ src/main/webapp/app/pages/home/index.js
@@ -8,14 +8,6 @@ angular.module('chhs').controller('homeCtrl', function ($http, $log) {
     home.account = resp.data;
   });
 
-  this.login = function () {
-    var authRequest = this.auth;
-    $log.info('Attempting login request...');
-    $http.post('/login', authRequest).then(function () {
-      document.location.reload(true);
-    }, function () {
-      $log.error('Error during login.');
-    });
-  };
+
 
 });
