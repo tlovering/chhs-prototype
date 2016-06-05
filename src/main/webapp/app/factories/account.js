35fa6e8879e57a86e25fcbd1281120ac476dd09e
rcharow <rachel.charow@portlandwebworks.com>
2016-06-05 11:23:46 -0400

    Throw errors in rejection function in Account factory

--- src/main/webapp/app/factories/account.js
+++ src/main/webapp/app/factories/account.js
@@ -6,14 +6,20 @@ angular.module('chhs').factory('Account', function ($http, $log) {
   var createAccount = function (accountInfo) {
     return $http.post('/api/account/register', accountInfo)
       .then(function (response) {
-          return response;
-      });
+        return response.data;
+      }, function (e) {
+        $log.error('Error creating user account.')
+        throw e;
+       });
   }
 
   var getUserAccount = function () {
     return $http.get('/api/account')
       .then(function (response) {
-        return response.data;
+        return response;
+      }, function (e) {
+        $log.error('Error getting user account.')
+        throw e;
       });
   }
 
