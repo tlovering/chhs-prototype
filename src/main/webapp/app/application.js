e43068b39eda9c95550c6833075a86ad11ad5d45
Joseph Descalzota <joseph.descalzota@portlandwebworks.com>
2016-06-05 09:49:53 -0400

    Remove intercept for 403. It's just for logging. Use dev tools.

--- src/main/webapp/app/application.js
+++ src/main/webapp/app/application.js
@@ -1,18 +1,6 @@
 'use strict';
 
 angular.module('chhs', ['ngCookies', 'ngRoute', 'uiGmapgoogle-maps'])
-  .config(function ($httpProvider) {
-    $httpProvider.interceptors.push(function () {
-      return {
-        responseError: function (rejection) {
-          if (rejection.status === 403) {
-            console.log('403 Error!');
-          }
-          return rejection;
-        }
-      };
-    });
-  })
   .run(function ($cookies, $http, $log) {
 
     if ($cookies.get('Token')) {
