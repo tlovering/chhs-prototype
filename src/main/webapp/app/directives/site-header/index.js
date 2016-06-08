af96a76ba2cb38a2a7e8b170865210988bbe4c0b
rcharow <rachel.charow@portlandwebworks.com>
2016-06-08 18:05:10 -0400

    Get current user in header controller to display on header

--- src/main/webapp/app/directives/site-header/index.js
+++ src/main/webapp/app/directives/site-header/index.js
@@ -4,11 +4,16 @@ angular.module('chhs').directive('siteHeader', function () {
   return {
     restrict: 'C',
     templateUrl: 'app/directives/site-header/index.jsp',
-    controller: function($scope, messagesFactory){
+    controller: function($scope, messagesFactory, Account){
       $scope.messageCount = null;
+      $scope.user = null;
 
       messagesFactory.getMessages().then(function(messages){
         $scope.messageCount = messages.length;
+      });
+
+      Account.getUserAccount().then(function(account){
+        $scope.user = account.firstName;
       });
 
     }
