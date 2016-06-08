da1ba0e7905893f99a09681d39b80391163f0a9f
rcharow <rachel.charow@portlandwebworks.com>
2016-06-07 20:11:50 -0400

    Select facility in map and table on click

--- src/main/webapp/app/directives/foster-home-locator/index.js
+++ src/main/webapp/app/directives/foster-home-locator/index.js
@@ -21,10 +21,10 @@ angular.module('chhs').directive('fosterHomeLocator', function (fosterHomesFacto
 
       var gMap;
 
-      var zoom = function () {
-        if (gMap && $scope.results && $scope.results.length > 0) {
+      var zoom = function (locations) {
+        if (gMap && locations && locations.length > 0) {
           var bounds = new google.maps.LatLngBounds();
-          _.each($scope.results, function (loc) {
+          _.each(locations, function (loc) {
             bounds.extend(new google.maps.LatLng({lat: loc.location.latitude, lng: loc.location.longitude}));
           });
           gMap.fitBounds(bounds);
@@ -38,30 +38,54 @@ angular.module('chhs').directive('fosterHomeLocator', function (fosterHomesFacto
 
       uiGmapIsReady.promise(1).then(function (instances) {
         gMap = instances[0].map;
-        zoom();
+        zoom($scope.results);
       });
 
       // Initialize async data
       fosterHomesFactory.getFosterHomes().then(function (fosterHomes) {
+        $scope.selectedFacility = null;
         $scope.results = fosterHomes;
-        zoom();
+        zoom($scope.results);
       });
 
       // Exposed functions
       $scope.searchFosterHomes = function (zip, proximity) {
         fosterHomesFactory.searchFosterHomes(zip, proximity).then(function (fosterHomes) {
+          $scope.selectedFacility = null;
           $scope.results = fosterHomes;
-          zoom();
+          zoom($scope.results);
         });
       };
 
       $scope.resetFosterHomes = function () {
         fosterHomesFactory.getFosterHomes().then(function (fosterHomes) {
+          $scope.selectedFacility = null;
           $scope.search = angular.copy(defaultSearchState);
           $scope.results = fosterHomes;
-          zoom();
+          zoom($scope.results);
         });
       };
+
+      $scope.mapClick = function(marker, eventName, model) {
+        $scope.selectedFacility = marker.key;
+        gMap.setZoom(16);
+        gMap.setCenter(marker.getPosition());
+
+        var top = $('#' + marker.key).position().top;
+        $('.foster-home-locator__search-results').scrollTop(top);
+
+      }
+
+      $scope.map.events = {
+        click: $scope.mapClick
+      }
+
+      $scope.selectFacility = function(location){
+        $scope.selectedFacility = location.facility_number;
+        var x = gMap.markers;
+        gMap.setZoom(16);
+      }
+
 
     }
   };
