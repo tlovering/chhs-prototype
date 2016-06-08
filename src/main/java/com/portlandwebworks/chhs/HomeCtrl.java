85577442ce66f5aaba11f958384cf7cbcb883939
Nick Stuart <nick@portlandwebworks.com>
2016-06-08 13:04:27 -0400

    Automatic redirect to index.jsp.

---
+++ src/main/java/com/portlandwebworks/chhs/HomeCtrl.java
@@ -0,0 +1,20 @@
+package com.portlandwebworks.chhs;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author nick
+ */
+@Controller
+@RequestMapping("/")
+public class HomeCtrl {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String redirect(){
+		return "redirect:/index.jsp";
+	}
+	
+}
